21e429f2bccccedbcfd13bbd884be05099e4f5f7
CNPJá! <cnpja@cnpja.com.br>
2020-03-02 14:12:04 -0300

    Ajustado formatação PHP

--- examples/php/curl.php
+++ examples/php/curl.php
@@ -1,41 +1,42 @@
 <?php
-$curl = curl_init();
 
-// Coloque aqui sua Chave de API
-$api_key = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+  $curl = curl_init();
 
-// Escolha o CNPJ para testar
-$tax_id = "33000167000101";
+  // Coloque aqui sua Chave de API
+  $api_key = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
 
-// Executa a chamada para API CNPJá!
-$cnpja_url="https://api.cnpja.com.br/companies/";
+  // Escolha o CNPJ para testar
+  $tax_id = "33000167000101";
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $cnpja_url . $tax_id,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => array(
-    "Authorization: " . $api_key
-  ),
-));
+  // Executa a chamada para API CNPJá!
+  $cnpja_url="https://api.cnpja.com.br/companies/";
 
-$response = curl_exec($curl);
-curl_close($curl);
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $cnpja_url . $tax_id,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "Authorization: " . $api_key
+    ),
+  ));
 
-echo "Resposta da API: <br>" . $response . "<br><br>";
-   
-// Decodifica o JSON de retorno
-$company = json_decode($response);
+  $response = curl_exec($curl);
+  curl_close($curl);
 
-// Acessa as propriedades que desejar
-echo "Razão Social: " . $company->name . "<br>";
-echo "Nome Fantasia: " . $company->alias . "<br>";
-echo "Tipo: " . $company->type . "<br>";
-// etc...
+  echo "Resposta da API: <br>" . $response . "<br><br>";
+    
+  // Decodifica o JSON de retorno
+  $company = json_decode($response);
+
+  // Acessa as propriedades que desejar
+  echo "Razão Social: " . $company->name . "<br>";
+  echo "Nome Fantasia: " . $company->alias . "<br>";
+  echo "Tipo: " . $company->type . "<br>";
+  // etc...
 
 ?>
